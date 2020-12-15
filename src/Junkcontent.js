4b6384628919f29c221353a2539f197175208080
CHRIS_LEE <chrismlee87@gmail.com>
2020-12-15 02:59:36 -0800

    navbar works

---
+++ src/Junkcontent.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './Junkcontent.css'
+
+function Junkcontent() {
+  return (
+    <div>
+      <main>
+        <h1>Check out my sweet animated sidebar</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque varius aliquet massa, eget efficitur felis porttitor quis. 
+          In cursus augue mauris. Quisque pulvinar purus sed neque facilisis sodales. Nam augue tortor, dictum non egestas in, viverra vitae 
+          neque. Cras porttitor porttitor libero a molestie. Mauris scelerisque a nisi a fringilla. Integer at sapien vulputate, eleifend 
+          ante et, dignissim enim. Phasellus urna nunc, elementum eget eros vitae, lobortis posuere est. Quisque ut leo ultrices, tincidunt 
+          odio vitae, pellentesque arcu. In ut sapien quis tellus mattis viverra. Proin dui nisl, sodales sit amet ipsum at, vestibulum 
+          condimentum nisi. Maecenas pulvinar augue sit amet sapien congue mattis. Vestibulum posuere fringilla diam. Vivamus condimentum 
+          molestie augue congue sodales. Suspendisse fermentum, erat eget viverra feugiat, tellus urna finibus velit, nec pulvinar mauris 
+          felis non enim. Quisque egestas vestibulum hendrerit.
+        </p>
+        <p>
+          Duis non cursus leo. Donec consectetur elit felis. Proin vitae feugiat risus, eget tristique nulla. Donec tristique ligula sit amet 
+          tortor cursus, et auctor nulla ornare. Praesent ligula ligula, malesuada eget urna non, sodales ultrices velit. Vivamus viverra, 
+          arcu fermentum mattis porta, mi mauris euismod neque, in porta mi magna ac justo. Ut metus nibh, rhoncus lobortis porta ultricies, 
+          volutpat quis magna. Mauris volutpat sapien non nibh iaculis dapibus. Quisque et est id neque euismod consequat. Nullam ultricies, 
+          risus nec imperdiet blandit, neque mi dignissim risus, quis maximus magna erat id lacus. Integer quis aliquam massa, ac finibus 
+          libero. Etiam vestibulum semper erat non lobortis. Praesent vel erat odio. Morbi pretium erat vitae tortor pretium, ac eleifend 
+          sapien accumsan.
+        </p>
+        <p>
+          Maecenas dictum arcu quis rutrum ornare. Sed molestie rutrum egestas. In fermentum, lacus at tincidunt aliquet, nulla dui malesuada 
+          enim, sed molestie velit urna ut tortor. Pellentesque volutpat quis elit nec mollis. Aliquam a massa leo. Sed id lacus sodales, 
+          gravida tellus eget, malesuada turpis. Duis lacus tortor, gravida eu neque quis, lacinia ultrices diam. Sed lacus ligula, interdum 
+          at felis vitae, dapibus sollicitudin sapien.
+        </p>
+        <p>
+          Maecenas dictum arcu quis rutrum ornare. Sed molestie rutrum egestas. In fermentum, lacus at tincidunt aliquet, nulla dui malesuada 
+          enim, sed molestie velit urna ut tortor. Pellentesque volutpat quis elit nec mollis. Aliquam a massa leo. Sed id lacus sodales, 
+          gravida tellus eget, malesuada turpis. Duis lacus tortor, gravida eu neque quis, lacinia ultrices diam. Sed lacus ligula, interdum 
+          at felis vitae, dapibus sollicitudin sapien.
+        </p>
+        <p>
+          Maecenas dictum arcu quis rutrum ornare. Sed molestie rutrum egestas. In fermentum, lacus at tincidunt aliquet, nulla dui malesuada 
+          enim, sed molestie velit urna ut tortor. Pellentesque volutpat quis elit nec mollis. Aliquam a massa leo. Sed id lacus sodales, 
+          gravida tellus eget, malesuada turpis. Duis lacus tortor, gravida eu neque quis, lacinia ultrices diam. Sed lacus ligula, interdum 
+          at felis vitae, dapibus sollicitudin sapien.
+        </p>
+        <p>
+          Maecenas dictum arcu quis rutrum ornare. Sed molestie rutrum egestas. In fermentum, lacus at tincidunt aliquet, nulla dui malesuada 
+          enim, sed molestie velit urna ut tortor. Pellentesque volutpat quis elit nec mollis. Aliquam a massa leo. Sed id lacus sodales, 
+          gravida tellus eget, malesuada turpis. Duis lacus tortor, gravida eu neque quis, lacinia ultrices diam. Sed lacus ligula, interdum 
+          at felis vitae, dapibus sollicitudin sapien.
+        </p>
+        <p>
+          Maecenas dictum arcu quis rutrum ornare. Sed molestie rutrum egestas. In fermentum, lacus at tincidunt aliquet, nulla dui malesuada 
+          enim, sed molestie velit urna ut tortor. Pellentesque volutpat quis elit nec mollis. Aliquam a massa leo. Sed id lacus sodales, 
+          gravida tellus eget, malesuada turpis. Duis lacus tortor, gravida eu neque quis, lacinia ultrices diam. Sed lacus ligula, interdum 
+          at felis vitae, dapibus sollicitudin sapien.
+        </p>
+        <p>
+          Maecenas dictum arcu quis rutrum ornare. Sed molestie rutrum egestas. In fermentum, lacus at tincidunt aliquet, nulla dui malesuada 
+          enim, sed molestie velit urna ut tortor. Pellentesque volutpat quis elit nec mollis. Aliquam a massa leo. Sed id lacus sodales, 
+          gravida tellus eget, malesuada turpis. Duis lacus tortor, gravida eu neque quis, lacinia ultrices diam. Sed lacus ligula, interdum 
+          at felis vitae, dapibus sollicitudin sapien.
+        </p>
+        <p>
+          Maecenas dictum arcu quis rutrum ornare. Sed molestie rutrum egestas. In fermentum, lacus at tincidunt aliquet, nulla dui malesuada 
+          enim, sed molestie velit urna ut tortor. Pellentesque volutpat quis elit nec mollis. Aliquam a massa leo. Sed id lacus sodales, 
+          gravida tellus eget, malesuada turpis. Duis lacus tortor, gravida eu neque quis, lacinia ultrices diam. Sed lacus ligula, interdum 
+          at felis vitae, dapibus sollicitudin sapien.
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export default Junkcontent
