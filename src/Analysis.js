d091308d34a68c89f783be465d79e88d420981fc
CHRIS_LEE <chrismlee87@gmail.com>
2021-01-09 11:44:56 -0800

    commit before breaking things

--- src/Analysis.js
+++ src/Analysis.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Analysis.css';
 import AlienImage from './images/4-alien.png';
+import StalkerImage from './images/5-stalker.jpg'
+import data from './movie-data.json';
 // Import AnalysisList Photo + Data to load into the src image
 
 function Analysis() {
+  // function defaultImage() {
+  //   document.getElementById('myImage').src="./images/4-alien.png"
+  // }
+
+  // function lightingAnalysis() {
+  //   document.getElementById('myImage').src="./images/4-alien.png"
+  // }
+
+  function showInfo(e) {
+    // e.target.setAttribute(src, {StalkerImage})
+  }
+
   return (
     <div>
       <h1 className="analysis__title">photography analysis:</h1>
@@ -21,7 +35,7 @@ function Analysis() {
         <p>summary text</p>
       </div>
       <div className="analysis__buttons">
-        <button>info</button>
+        <button onClick={showInfo}>info</button>
         <button>overview</button> {/* Pulls TMDB API to make new text */}
         <button>show all</button> {/* Shows everything all buttons go yellow */}
         <button>lighting</button> {/* Photo changes to lighting */}
--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Nav from './Nav';
 import About from './About';
 import Analysis from './Analysis';
 import Compare from './Compare';
-import Junkcontent from './Junkcontent';
 import Footer from './Footer';
 
 function App() {
